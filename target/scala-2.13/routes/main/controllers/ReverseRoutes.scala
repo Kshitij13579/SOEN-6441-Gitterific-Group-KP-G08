058651e8bf5293a56b6d42ebe97d56c63b1b0002
mrynalrai <mrynalrai@gmail.com>
2021-11-13 15:57:36 -0500

    added topicPage view

--- target/scala-2.13/routes/main/controllers/ReverseRoutes.scala
+++ target/scala-2.13/routes/main/controllers/ReverseRoutes.scala
@@ -1,6 +1,6 @@
 // @GENERATOR:play-routes-compiler
 // @SOURCE:D:/Concordia/SOEN 6441 W 2212/Project/SOEN-6441-Gitterific-Group-KP-G08/conf/routes
-// @DATE:Sat Nov 13 14:59:03 EST 2021
+// @DATE:Sat Nov 13 15:52:00 EST 2021
 
 import play.api.mvc.Call
 
