6bebb2577b103c2584d5ab54bd6d683d6df74735
siddharthajha07 <siddharthajha07@gmail.com>
2021-11-16 19:58:55 -0500

    added spaces to make code readable

--- test/model/RepositoryTest.java
+++ test/model/RepositoryTest.java
@@ -15,17 +15,14 @@ public class RepositoryTest {
 	    ArrayList<String> topics = new ArrayList<String>();
 	    topics.add("java");
 		Repository repository=new Repository("er1","s228","https://api.github.com/repos/er1/s228/issues{/number}","https://api.github.com/repos/er1/s228/commits%7B/sha%7D%22",topics);
-		
 		//assertFalse(repository.login.isEmpty());
 		assertEquals("er1",repository.getLogin());
 		//assertFalse(repository.name.isEmpty());
 		assertEquals("s228",repository.getName());
 		assertEquals("https://api.github.com/repos/er1/s228/issues{/number}",repository.issues_url);
 		//assertFalse(repository.issues_url.isEmpty());
-		
 		//assertFalse(repository.commits_url.isEmpty());
-		assertEquals("https://api.github.com/repos/er1/s228/commits%7B/sha%7D%22",repository.getCommits_url());
-				
+		assertEquals("https://api.github.com/repos/er1/s228/commits%7B/sha%7D%22",repository.getCommits_url());	
 	}
 	
 	@Test
